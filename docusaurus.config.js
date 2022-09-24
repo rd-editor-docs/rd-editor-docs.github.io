46a78b3a73955624f4b85a782a87de45156a2357
WillFlame <flameyanai@gmail.com>
2022-09-24 21:25:59 +0800

    Fix config baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Level Editor Tutorial',
   tagline: 'A tutorial that covers everything in the level editor, from cues to surgery.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://rd-editor-docs.github.io/rd-editor-docs',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
