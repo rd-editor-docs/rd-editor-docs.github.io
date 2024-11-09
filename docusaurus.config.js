0a77a0bf4d8852944723bc80f0c9f2ed0cd902e9
raf13lol <79861112+raf13lol@users.noreply.github.com>
2024-11-09 19:12:58 +0000

    copyright is not going to "my project"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear} RD Level Editor Tutorial. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
